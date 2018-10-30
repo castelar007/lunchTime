5b85e493a26c7d2058ae89aab4f91f71deee73a1
castelar007 <castelar007@gmail.com>
2018-10-30 17:07:45 -0600

    oct 30 commit

--- src/pages/basket/basket.ts
+++ src/pages/basket/basket.ts
@@ -7,6 +7,9 @@ import {  NavController, NavParams } from 'ionic-angular';
   templateUrl: 'basket.html',
 })
 export class BasketPage {
+  aceptar = true;
+  togglePago = false;
+  tarjetas = [{number:'8888-8888-8888-8888',selected:true},{number:'8888-888888-88888',selected:false},{number:'8888-8888-7788-8888',selected:false},{number:'8888-888778-88888',selected:false}];
   pagos = [{name:'Efectivo',active:true},{name:'Tarjeta',active:false},{name:'Otro',active:false}];
   items = [{imgUrl:'assets/imgs/test.png',cant:0},{imgUrl:'assets/imgs/test.png',cant:0},{imgUrl:'assets/imgs/test.png',cant:0},{imgUrl:'assets/imgs/test.png',cant:0},{imgUrl:'assets/imgs/test.png',cant:0}];
   constructor(public navCtrl: NavController, public navParams: NavParams) {
@@ -27,5 +30,21 @@ export class BasketPage {
       element.active = false;
     });
     pago.active = true;
+    if(pago.name == 'Tarjeta'){
+      this.togglePago = true;
+    }else if(pago.name == 'Otro'){
+      this.togglePago = true;
+    }else {
+      this.togglePago = false;
+    }
+
+  }
+  acceptClick(){
+    this.togglePago = false;
+  }
+  enableAceptar(){
+    console.log('changed');
+    
+    this.aceptar = false;
   }
 }
