45deba46af6492109dc4adfdbf38247c28d2d395
castelar007 <castelar007@gmail.com>
2018-11-01 17:20:36 -0600

    commit 1nov

--- src/pages/basket/basket.ts
+++ src/pages/basket/basket.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,NgZone } from '@angular/core';
 import {  NavController, NavParams } from 'ionic-angular';
 
 
@@ -7,12 +7,14 @@ import {  NavController, NavParams } from 'ionic-angular';
   templateUrl: 'basket.html',
 })
 export class BasketPage {
+ 
+  isOtherSelected=false;
   aceptar = true;
   togglePago = false;
   tarjetas = [{number:'8888-8888-8888-8888',selected:true},{number:'8888-888888-88888',selected:false},{number:'8888-8888-7788-8888',selected:false},{number:'8888-888778-88888',selected:false}];
   pagos = [{name:'Efectivo',active:true},{name:'Tarjeta',active:false},{name:'Otro',active:false}];
   items = [{imgUrl:'assets/imgs/test.png',cant:0},{imgUrl:'assets/imgs/test.png',cant:0},{imgUrl:'assets/imgs/test.png',cant:0},{imgUrl:'assets/imgs/test.png',cant:0},{imgUrl:'assets/imgs/test.png',cant:0}];
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(private zone: NgZone,public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
@@ -26,18 +28,39 @@ export class BasketPage {
     item.cant = item.cant - 1;
   }
   pagoClick(pago){
+    
     this.pagos.forEach(element => {
       element.active = false;
     });
     pago.active = true;
-    if(pago.name == 'Tarjeta'){
-      this.togglePago = true;
-    }else if(pago.name == 'Otro'){
-      this.togglePago = true;
-    }else {
-      this.togglePago = false;
-    }
-
+    let whiteHeader = document.querySelector('.whiteHeader');
+    let sliderContainer = document.querySelector('.sliderContainer');
+    let activeFunc = function(){
+        if(pago.name == 'Tarjeta'){
+         sliderContainer.classList.add('slow');
+        }else if(pago.name == 'Otro'){
+          sliderContainer.classList.add('slow');
+          
+        }else {
+          sliderContainer.classList.remove('slow');
+        }
+        
+        
+        
+        whiteHeader.removeEventListener("transitionend", activeFunc, true); 
+      }
+      whiteHeader.addEventListener("transitionend", activeFunc, true);
+      if(pago.name == 'Tarjeta'){
+        this.isOtherSelected = false;
+        this.togglePago = true;
+      }else if(pago.name == 'Otro'){
+        this.isOtherSelected = true;
+        this.togglePago = true;
+      }else {
+        this.togglePago = false;
+      }
+      
+   
   }
   acceptClick(){
     this.togglePago = false;
